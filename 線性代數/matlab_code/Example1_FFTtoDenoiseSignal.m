9515fd9d5761d200e7e3120bfa285540987a0a46
unknown <wangia@solab.me.ntu.edu.tw>
2023-12-25 21:33:43 +0800

    2023/12/25 Merry Chrismas

---
+++ 線性代數/matlab_code/Example1_FFTtoDenoiseSignal.m
@@ -0,0 +1,28 @@
+dt = .001;
+t = 0:dt:1;
+f = sin(2*pi*50*t) + sin(2*pi*120*t); % Sum of 2 frequencies
+f = f + 2.5*randn(size(t)); % Add some noise
+%% Compute the Fast Fourier Transform FFT
+n = length(t);
+fhat = fft(f,n); % Compute the fast Fourier transform
+PSD = fhat.*conj(fhat)/n; % Power spectrum (power per freq)
+freq = 1/(dt*n)*(0:n); % Create x-axis of frequencies in Hz
+L = 1:floor(n/2); % Only plot the first half of freqs
+%% Use the PSD to filter out noise
+indices = PSD>100; % Find all freqs with large power
+PSDclean = PSD.*indices; % Zero out all others
+fhat = indices.*fhat; % Zero out small Fourier coeffs. in Y
+ffilt = ifft(fhat); % Inverse FFT for filtered time signal
+%% PLOTS
+subplot(3,1,1)
+plot(t,f,'r','LineWidth',1.2), hold on
+plot(t,f,'k','LineWidth',1.5)
+legend('Noisy','Clean')
+subplot(3,1,2)
+plot(t,f,'k','LineWidth',1.5), hold on
+plot(t,ffilt,'b','LineWidth',1.2)
+legend('Clean','Filtered')
+subplot(3,1,3)
+plot(freq(L),PSD(L),'r','LineWidth',1.5), hold on
+plot(freq(L),PSDclean(L),'-b','LineWidth',1.2)
+legend('Noisy','Filtered')
