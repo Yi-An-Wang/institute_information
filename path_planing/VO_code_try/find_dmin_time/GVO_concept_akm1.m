cb7523a2901507608fecd3a4ad9789ae5f20ce06
Yi-An-Wang <wangia@solab.me.ntu.edu.tw>
2024-05-13 08:26:25 +0800

    204/05/13-1

--- path_planing/VO_code_try/find_dmin_time/GVO_concept_akm1.m
+++ path_planing/VO_code_try/find_dmin_time/GVO_concept_akm1.m
@@ -1,23 +1,21 @@
 r_A=1;
 r_B=1;
-p_B=[3;5];
-v_B=[-2;-2];
+p_B=[5;5];
+v_B=[-0.2;0];
 v_max=6;
 v_min=0;
 steering_max=pi/6;
 steering_min=-pi/6;
-collision_information=zeros(4,241*241);
+collision_information=zeros(5,241*210);
 
 n=1;
-for v=v_min:0.025:v_max
-    for psi=steering_min:pi/720:steering_max
-        [t,d_min,exitflag]=find_d_min(psi,v,p_B,v_B);
-%         v=[v_x;psi];
-%         t=p_B'*(v-v_B)/((v-v_B)'*(v-v_B));
-%         p_shortest=t*v-p_B-t*v_B;
-%         d_min=(p_shortest(1)^2+p_shortest(2)^2)^0.5;
+v=v_min:0.025:v_max;
+psi=steering_min:0.005:steering_max;
+for ii=1:241
+    for jj=1:210
+        [t,d_min,exitflag]=find_d_min(psi(jj),v(ii),p_B,v_B);
         if d_min<=(r_A+r_B)
-            collision_information(:,n)=[v;psi;t;d_min];
+            collision_information(:,n)=[v(ii);psi(jj);t;d_min;exitflag];
             n=n+1;
         end
     end
@@ -29,3 +27,50 @@ for ii=1:size(collision_information,2)
         break
     end
 end
+
+%%
+color=max(collision_information(3,:))*ones(1,size(collision_information,2))-collision_information(3,:);
+figure(1)
+axis([v_min v_max steering_min steering_max])
+hold on
+scatter(collision_information(1,:),collision_information(2,:),[],color,"filled")
+hold off
+
+%%
+figure(2)
+axis([v_min-3 v_max+1 v_min-3 v_max+1])
+hold on
+vx=collision_information(1,:).*cos(collision_information(1,:).*collision_information(3,:).*tan(collision_information(2,:)));
+vy=collision_information(1,:).*sin(collision_information(1,:).*collision_information(3,:).*tan(collision_information(2,:)));
+scatter(vx,vy,[],color,"filled")
+
+A_x=r_A*cos(linspace(0,2*pi));
+A_y=r_A*sin(linspace(0,2*pi));
+patch("XData",A_x,"YData",A_y,"FaceColor","green")
+
+B_x=p_B(1)*ones(1,100)+r_B*cos(linspace(0,2*pi));
+B_y=p_B(2)*ones(1,100)+r_B*sin(linspace(0,2*pi));
+patch("XData",B_x,'YData',B_y,"FaceColor","red")
+
+quiver(p_B(1),p_B(2),v_B(1),v_B(2),"b");
+text(p_B(1),p_B(2),'B','Color','black','FontSize',20)
+text(0,0,'A',"Color",'black','FontSize',20)
+hold off
+
+%%
+figure(3)
+axis([-5 6 -5 6])
+hold on
+patch("XData",A_x,"YData",A_y,"FaceColor","green")
+t_p=linspace(0,max(collision_information(3,:)),5);
+P_B_hat=p_B+v_B.*t_p;
+for ii=1:5
+    B_hat_x=P_B_hat(1,ii)*ones(1,100)+(r_B+r_A)*cos(linspace(0,2*pi));
+    B_hat_y=P_B_hat(2,ii)*ones(1,100)+(r_B+r_A)*sin(linspace(0,2*pi));
+    s=patch("XData",B_hat_x,"YData",B_hat_y,"FaceColor","magenta");
+    alpha(s,0.5+ii*0.1)
+end
+p_Ax=1./tan(collision_information(2,:)).*sin(collision_information(1,:).*tan(collision_information(2,:)).*collision_information(3,:));
+p_Ay=-1./tan(collision_information(2,:)).*cos(collision_information(1,:).*tan(collision_information(2,:)).*collision_information(3,:))+1./tan(collision_information(2,:));
+scatter(p_Ax,p_Ay,[],color,"filled")
+hold off
--- path_planing/VO_code_try/find_dmin_time/find_d_min.m
+++ path_planing/VO_code_try/find_dmin_time/find_d_min.m
@@ -3,4 +3,5 @@ obj=@(t) obj_function1(t,phi,v,p_B,v_B);
 t0=0;
 A=-1;
 b=0;
-[t,d_min,exitflag]=fmincon(obj,t0,A,b);
+[t,d_sqr,exitflag]=fmincon(obj,t0,A,b);
+d_min=d_sqr^0.5;
