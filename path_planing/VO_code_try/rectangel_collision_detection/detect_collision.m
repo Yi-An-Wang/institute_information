caf6cd7776fed5d6b861a56bb1d728640cda8bdc
unknown <wangia@solab.me.ntu.edu.tw>
2024-07-08 09:46:43 +0800

    2024/07/08-1

--- path_planing/VO_code_try/rectangel_collision_detection/detect_collision.m
+++ path_planing/VO_code_try/rectangel_collision_detection/detect_collision.m
@@ -1,20 +1,20 @@
-function detection=detect_collision(GBM1,GBM2)
-inflate=GBM1.r+GBM2.r;
-distance_vector=GBM1.center_position-GBM2.center_position;
+function detection=detect_collision(obj1,obj2)
+inflate=obj1.r+obj2.r;
+distance_vector=obj1.center_position-obj2.center_position;
 distance=(distance_vector'*distance_vector)^0.5;
 detection=0;
 a_sequence=[4 1 2 3];
 b_sequence=[2 3 4 1];
 if distance<inflate
     for ii=1:5
-        d_point=GBM2.d_points(:,ii);
+        d_point=obj2.d_points(:,ii);
         in=[0 0 0 0];
         for jj=1:4
-            a=GBM1.d_points(:,a_sequence(jj))-GBM1.d_points(:,jj);
+            a=obj1.d_points(:,a_sequence(jj))-obj1.d_points(:,jj);
             a_length=(a'*a)^0.5;
-            b=GBM1.d_points(:,b_sequence(jj))-GBM1.d_points(:,jj);
+            b=obj1.d_points(:,b_sequence(jj))-obj1.d_points(:,jj);
             b_length=(b'*b)^0.5;
-            c=d_point-GBM1.d_points(:,jj);
+            c=d_point-obj1.d_points(:,jj);
             c_length=(c'*c)^0.5;
             th1=a'*c/(a_length*c_length);
             th2=b'*c/(b_length*c_length);
@@ -30,14 +30,14 @@ if distance<inflate
     end
     if detection==0
         for ii=1:5
-            d_point=GBM1.d_points(:,ii);
+            d_point=obj1.d_points(:,ii);
             in=[0 0 0 0];
             for jj=1:4
-                a=GBM2.d_points(:,a_sequence(jj))-GBM2.d_points(:,jj);
+                a=obj2.d_points(:,a_sequence(jj))-obj2.d_points(:,jj);
                 a_length=(a'*a)^0.5;
-                b=GBM2.d_points(:,b_sequence(jj))-GBM2.d_points(:,jj);
+                b=obj2.d_points(:,b_sequence(jj))-obj2.d_points(:,jj);
                 b_length=(b'*b)^0.5;
-                c=d_point-GBM2.d_points(:,jj);
+                c=d_point-obj2.d_points(:,jj);
                 c_length=(c'*c)^0.5;
                 th1=a'*c/(a_length*c_length);
                 th2=b'*c/(b_length*c_length);
