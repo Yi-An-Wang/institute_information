b809a77f4e3a84dbde5798965c9cece3abf84aaa
Yi-An Wang <Wangia@solab.me.ntu.edu.tw>
2024-12-02 08:55:34 +0800

    2024/12/2-1

--- path_planing/VO_code_try/rectangel_collision_detection/Astar_basic.m
+++ path_planing/VO_code_try/rectangel_collision_detection/Astar_basic.m
@@ -1,10 +1,10 @@
 rng('default');
 map1=mapClutter;
-map2 = mapMaze;
+map2 = mapMaze(10,3,'MapSize',[30 30]);
 
 %% Astar
 planner=plannerAStarGrid(map2);
-start=[240 10];
+start=[140 10];
 goal=[10 140];
 way_point=plan(planner,start,goal);
 figure(1)
